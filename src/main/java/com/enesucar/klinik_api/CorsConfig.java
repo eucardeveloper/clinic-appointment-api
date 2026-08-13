747a64da2cbd76df184b7fff3b4233069a609e83
Enes <eucardeveloper@gmail.com>
2026-08-13 21:25:12 +0300

    Add CORS config

--- src/main/java/com/enesucar/klinik_api/CorsConfig.java
+++ src/main/java/com/enesucar/klinik_api/CorsConfig.java
@@ -1,4 +1,22 @@
-package com.enesucar.klinik_api;
+package com.enesucar.klinik;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
